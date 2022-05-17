ca8abc186b2aadb0ff1555a2ebe6a19412b41762
lucas-esc <102486519+lucas-esc@users.noreply.github.com>
2022-05-16 23:13:26 -0300

    Update Complementing a Strand of DNA.py

--- Complementing a Strand of DNA.py
+++ Complementing a Strand of DNA.py
@@ -1,23 +1,29 @@
-data = "AAAACCCGGT"
-
-split_data = list(data)
-
-def transc_data(sequence):
+#Function to compliment the previous inversed sequence 
+def compli_data(sequence):
     for i in range(len(sequence)):
         if sequence[i] == "A":
-            sequence[i] = "c"
-
-        if sequence[i] == "C":
-            sequence[i] = "a"
-
-        if sequence[i] == "G":
             sequence[i] = "t"
 
-        if sequence[i] == "T":
+        if sequence[i] == "C":
             sequence[i] = "g"
+
+        if sequence[i] == "G":
+            sequence[i] = "c"
+
+        if sequence[i] == "T":
+            sequence[i] = "a"
             
     return sequence
 
-new_data = transc_data(split_data)
-new_str = ''.join(new_data)
-print(new_str.upper())
+#Inicial data
+data = "AAAACCCGGT"
+
+#Inverse the sequence
+split_data = list(data)
+split_data.reverse()
+
+#Complement the sequence using the function
+compli_sequen = compli_data(split_data)
+sequen_str = ''.join(compli_sequen)
+sequence_compliment = sequen_str.upper()
+print(sequence_compliment)
